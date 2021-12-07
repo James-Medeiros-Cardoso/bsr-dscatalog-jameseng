7cdde326c0c86867a439b8b685eda11afd75e211
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2021-12-07 01:21:12 -0300

    02-21 Exercício com repository

---
+++ backend/src/test/java/com/jameseng/dscatalog/tests/Factory.java
@@ -0,0 +1,21 @@
+package com.jameseng.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.jameseng.dscatalog.dto.ProductDTO;
+import com.jameseng.dscatalog.entities.Category;
+import com.jameseng.dscatalog.entities.Product;
+
+public class Factory {
+
+	public static Product createProduct() {
+		Product product=new Product(1L, "Phone", "Good Phone", 800.0, "http://img.com/img.png", Instant.parse("2020-02-20T03:00:00Z"));
+		product.getCategories().add(new Category(2L, "Electronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product=createProduct();
+		return new ProductDTO(product, product.getCategories());	
+	}
+}
