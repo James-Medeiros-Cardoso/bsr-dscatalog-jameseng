a016eb7ff005b818c45d6e5fcaa8f31096f26bcc
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2022-12-28 00:39:27 -0300

    Bootstrap theme customization

--- frontweb/src/index.tsx
+++ frontweb/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@popperjs/core';
+import 'bootstrap/js/dist/collapse';
+//import 'bootstrap/dist/css/bootstrap.css'; // APAGADO
 import App from './App';
 
 ReactDOM.render(
