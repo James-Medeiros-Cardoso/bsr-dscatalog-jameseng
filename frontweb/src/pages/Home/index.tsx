0c36383c07febefc44620dc07aa85cd7771c93fe
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2022-12-29 02:16:15 -0300

    aula 20 OK

---
+++ frontweb/src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import NavBar from 'components/NavBar';
+
+import './styles.css';
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="home-container">
+        <div className="home-card">
+            <div className="home-content-container">
+                <h1>Conheça o melhor catálogo de produtos.</h1>
+            </div>
+            <div className="home-image-container">
+                <MainImage />
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
